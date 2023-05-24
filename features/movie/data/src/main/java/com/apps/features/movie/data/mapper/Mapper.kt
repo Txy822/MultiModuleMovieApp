037cfff444365872b43dbc9f4906970e25e940dc
Tesfahun Yohanis-Wolleli <y.w.tesfahun@gmail.com>
2023-05-24 19:59:02 +0100

    mapper updated

--- features/movie/data/src/main/java/com/apps/features/movie/data/mapper/Mapper.kt
+++ features/movie/data/src/main/java/com/apps/features/movie/data/mapper/Mapper.kt
@@ -3,14 +3,7 @@ package com.apps.features.movie.data.mapper
 import com.apps.features.movie.domain.model.Movie
 import com.core.network.dto.MovieDto
 import com.core.network.dto.MovieListResponse
-/*
-fun MovieListResponse.toDomainMovieList(): List<Movie> {
-    return this.results.map {
-        Movie(makeFullUrl(it.posterPath)
-        )
-    }
-}
-*/
+
 fun MovieDto.toMovieModel(): Movie =
     Movie(imgUrl = makeFullUrl(posterPath),
         adult = adult,
