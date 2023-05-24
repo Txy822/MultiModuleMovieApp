a3f92a02e4b792b26b05a9d9cf35b4678fb26e6e
Tesfahun Yohanis-Wolleli <txy822@cs.bham.ac.uk>
2023-05-24 18:10:25 +0100

    Profile Screen updated

--- features/movie/ui/src/main/java/com/apps/feature/movie/ui/screens/ProfileScreen.kt
+++ features/movie/ui/src/main/java/com/apps/feature/movie/ui/screens/ProfileScreen.kt
@@ -1,36 +1,38 @@
 package com.apps.feature.movie.ui.screens
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfileScreen() {
-    val userProfile = remember { Profile() }
+   // val userProfile = remember { Profile() }
+    val userProfile = remember { getDummyUserProfile() }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(16.dp)
     ) {
         Image(
-            painter = painterResource(id = com.core.common.R.drawable.heart),
+            painter = painterResource(id = com.core.common.R.drawable.person),
             contentDescription = "Profile Image",
             modifier = Modifier
                 .size(150.dp)
@@ -50,10 +52,41 @@ fun ProfileScreen() {
             text = userProfile.email,
             style = MaterialTheme.typography.subtitle1
         )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = userProfile.bio,
+            style = MaterialTheme.typography.body1
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        val customButtonColors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Black,
+            contentColor = Color.White
+        )
+        Button(
+            onClick = { /* Handle button click */ },
+            modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(15.dp)),
+            colors =customButtonColors
+        ) {
+            Text(text = "Edit Profile", color= Color.White)
+        }
     }
 }
 
-data class Profile(
-    val name:String ="Tesfahun",
-    val email:String  ="abc1@gmail.com") {
+
+data class UserProfile(
+    val name: String,
+    val email: String,
+    val bio: String
+)
+
+fun getDummyUserProfile(): UserProfile {
+    return UserProfile(
+        name = "John Doe",
+        email = "johndoe@example.com",
+        bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac leo scelerisque, commodo est nec, semper mi."
+    )
 }
