5ddc9f15fd5b43d280adf42dbaa95abb7123dcdb
Tesfahun Yohanis-Wolleli <y.w.tesfahun@gmail.com>
2023-05-25 00:36:25 +0100

    Viewmodel updated

--- features/favorite/ui/src/main/java/com/sample/features/favorite/ui/viewmodel/FavoriteMovieViewModel.kt
+++ features/favorite/ui/src/main/java/com/sample/features/favorite/ui/viewmodel/FavoriteMovieViewModel.kt
@@ -2,34 +2,52 @@ package com.sample.features.favorite.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.core.common.Resource
 import com.sample.features.favorite.domain.model.FavoriteMovieModel
 import com.sample.features.favorite.domain.repo.FavoriteMovieRepository
+import com.sample.features.favorite.domain.use_case.GetFavoriteMovieUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class FavoriteMovieViewModel(private val repository: FavoriteMovieRepository) : ViewModel() {
+class FavoriteMovieViewModel @Inject constructor(
+    private val favoriteMovieUseCase: GetFavoriteMovieUseCase
+) : ViewModel() {
     private val _favoriteMovies = MutableStateFlow<List<FavoriteMovieModel>>(emptyList())
     val favoriteMovies: StateFlow<List<FavoriteMovieModel>> = _favoriteMovies
 
     init {
+
+    }
+
+    fun getFavorites() {
         viewModelScope.launch {
-            repository.favoriteMovies.collect {
-                _favoriteMovies.value = it
+            favoriteMovieUseCase.getFavoriteMovies().collect { result ->
+                when (result) {
+                    is Resource.Success -> {
+                        _favoriteMovies.value = result.data!!
+                    }
+
+                    is Resource.Loading -> {}
+                    is Resource.Error -> {}
+                }
             }
         }
     }
 
-    fun addMovieToFavorites(movie: Movie) {
+
+    fun addMovieToFavorites(movie: FavoriteMovieModel) {
         viewModelScope.launch {
-            repository.insertMovie(movie)
+            favoriteMovieUseCase.insertMovie(movie)
         }
     }
 
-    fun removeMovieFromFavorites(movie: Movie) {
+    fun removeMovieFromFavorites(movie: FavoriteMovieModel) {
         viewModelScope.launch {
-            repository.deleteMovie(movie)
+            favoriteMovieUseCase.deleteMovie(movie)
         }
     }
 }
